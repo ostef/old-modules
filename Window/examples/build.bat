a875d5927f35f735172a4686fa453c55f0483bb3
ostef <47391841+ostef@users.noreply.github.com>
2022-05-22 17:23:51 +0200

    Window: updated example build script for latest Jai beta.

--- Window/examples/build.bat
+++ Window/examples/build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-jai -no_dce -meta Meta/Build -import_dir ../.. -import_dir c:/jai/modules custom_caption.jai -- %*
+jai -no_dce -no_check custom_caption.jai -- %* --- import_dir ../.. import_dir c:/jai/modules
